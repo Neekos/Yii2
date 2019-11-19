4a6672a73617c1c4ee516ffca3322ce775497925
Андрей Спиридонов <and1997310366@mail.ru>
2019-11-19 17:43:37 +0700

    Изучаю структуру Yii2

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,8 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionFirst(){
+        return $this->render('first');
+    }
 }
